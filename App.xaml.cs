44bf3226931b0c2eae8620f5129548a346837d21
Мищенко Олег Владимирович <MischenkoOV@corp.loc>
2018-07-04 22:21:13 +0300

    add all files

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Markup.Xaml;
+
+namespace ImgGetCoordinates
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+   }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media.Imaging;
+using System.Collections.Generic;
+
+namespace ImgGetCoordinates
+{
+    public class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public async void OnButtonClicked(object sender, RoutedEventArgs args)
+        {
+            var ofd = new OpenFileDialog();
+            ofd.Title = "Choose a picture to load";
+            ofd.Filters.Add(new FileDialogFilter { Name = "Images", Extensions = new List<string> { "png", "bmp", "jpg", "jpeg" } });
+            var selectedFilePath = await ofd.ShowAsync();
+
+            if (selectedFilePath.Length > 0)
+            {
+                var img = this.Find<Image>("Img1");
+                img.Source = new Bitmap(selectedFilePath[0]);
+            }
+            //Content.
+            //var context = DataContext as HelloViewModel;
+            //context.Result = "Hi " + context.Name + " !";
+        }
+    }
+}
